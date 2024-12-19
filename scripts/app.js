aef5cca56e77a51fc767840423cd8bb4a008d443
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-12-20 00:38:46 +0200

    finalized logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,11 @@
-// screen value = [{value : "+" or "*" or 12 ..., type:"number" or "operator" }]
-let screen_value = [];
-let temp_number = "";
+// global variables
+let memory = []; // where we store all the input
+let temp_number = ""; // number to be pushed into memory
 let is_postfix = true;
 let is_showing_results = false;
 let is_darkmode = true;
 
+// adding listeners to components
 document
   .querySelectorAll(".number")
   .forEach((elem) => elem.addEventListener("click", numberClick));
@@ -23,35 +24,79 @@ document
 
 document.getElementById("=").addEventListener("click", equalClick);
 
-document.getElementById("togglemode").addEventListener("click", toggleMode)
-
-
+document.getElementById("toggle-mode").addEventListener("change", toggleThemeMode);
 
 document
   .querySelectorAll(".button")
   .forEach((elem) => elem.addEventListener("click", blink));
-document.getElementById("close").addEventListener("click", closeButton)
 
-document.getElementById("open").addEventListener("click", openButton)
+document.getElementById("close").addEventListener("click", closeButton);
 
+document.getElementById("open").addEventListener("click", openButton);
+
+function closeButton() {
+  document.getElementById("tutorial").style.display = "none";
+}
+
+function openButton() {
+  document.getElementById("tutorial").style.display = "flex";
+}
+
+function blink() {
+  const blinked = this.id;
+  const blink_color = document.getElementById(blinked);
+
+  blink_color.classList.add("blink")
+
+  //remove 
+  setTimeout(() => {
+  blink_color.classList.remove("blink")
+  }, 200); 
+}
 
 function display(value) {
   const screen = document.getElementById("display");
   const intial_value = screen.innerHTML;
+
   screen.innerHTML = intial_value + value;
 }
+
 function displayResult(result) {
   const screen = document.getElementById("display");
   screen.innerHTML = result;
+}
+
+function clear() {
+  memory = [];
+  temp_number = "";
+  const screen = document.getElementById("display");
+  screen.innerHTML = "";
+  is_showing_results = false;
+}
+
+function toggleModeClick() {
+  is_postfix = !is_postfix;
+  clear();
+}
+
+function toggleThemeMode() {
+  is_darkmode = !is_darkmode;
+  if (is_darkmode) {
+    document.getElementById("calculator").classList.remove("light");
+  } else {
+    document.getElementById("calculator").classList.add("light");
+  }
 }
 
 function numberClick() {
   if (is_showing_results) {
     clear();
   }
-  if (!is_postfix && screen_value.length == 0) {
+
+  if (!is_postfix && memory.length == 0) {
     return;
   }
+
   const clicked = this.id;
   temp_number += clicked;
   display(clicked);
@@ -61,31 +106,18 @@ function operatorClick() {
   if (is_showing_results) {
     clear();
   }
-  if (is_postfix && screen_value.length < 2) {
+
+  if (is_postfix && memory.length < 2) {
     return;
   }
+
   if (temp_number != "") {
     return;
   }
+
   const clicked = this.id;
   display(clicked + " ");
-  screen_value.push({ value: clicked, type: "operator" });
-  console.log(screen_value);
-}
-
-function toggleModeClick() {
-  is_postfix = !is_postfix;
-  clear();
-}
-
-function blink() {
-  console.log("blink");
-  const blinked = this.id;
-  const blink_color = document.getElementById(blinked);
-  blink_color.style.backgroundColor = "gray";
-  setTimeout(() => {
-    blink_color.style.backgroundColor = "#171717";
-  }, 200);
+  memory.push({ value: clicked, type: "operator" });
 }
 
 function actionClick() {
@@ -94,20 +126,19 @@ function actionClick() {
     // when enter is pressed we push the temp number into the array
     // when enter is emptyfF
     if (temp_number != "") {
-      screen_value.push({ value: parseFloat(temp_number), type: "number" });
+      memory.push({ value: parseFloat(temp_number), type: "number" });
       temp_number = "";
       const screen = document.getElementById("display");
       screen.innerHTML += " ";
     }
-    console.log(screen_value);
   } else if (clicked == "delete") {
-    if (screen_value.length == 0) {
+    if (memory.length == 0 && temp_number == "") {
       return;
     }
     // if (tempnmber ==>  empty) then I can remove last item from array and last item from diaplay
     // if temp is not empty then I can oly remove from display
     if (temp_number == "") {
-      screen_value.pop();
+      memory.pop();
       const screen = document.getElementById("display");
       const separate_entries = screen.innerHTML.split(" ");
       separate_entries.splice(separate_entries.length - 2, 1);
@@ -123,57 +154,14 @@ function actionClick() {
   }
 }
 
-function clear() {
-  screen_value = [];
-  temp_number = "";
-  const screen = document.getElementById("display");
-  screen.innerHTML = "";
-  is_showing_results = false;
-}
-
-function equalClick() {
-  // the operators have to be less than the numbers by 1
-  const numbers_count = screen_value.filter(
-    (elem) => elem.type == "number"
-  ).length;
-  const operators_count = screen_value.length - numbers_count;
-  if (operators_count != numbers_count - 1) {
-    return;
-  }
-
-  if (screen_value.length == 0) {
-    console.log("if the user has not entred anything yet");
-    return;
-  }
-  if (temp_number != "") {
-    console.log("if the user has not pressed enter while inputing a number");
-    return;
-  }
-  if (is_postfix && screen_value[screen_value.length - 1].type == "number") {
-    console.log("it is a postfix and the last item is a number");
-    return;
-  }
-  if (!is_postfix && screen_value[screen_value.length - 1].type == "operator") {
-    console.log("it is a prefix and the last item is an operator");
-    return;
-  }
-  if (is_postfix) {
-    const result = evaluatePostFix();
-    displayResult(result);
-  } else {
-    const result = evaluatePreFix();
-    displayResult(result);
-  }
-  is_showing_results = true;
-}
 
 function evaluatePostFix() {
   const postfix_stack = [];
   //we nee to loop over the array of values
-  for (let i = 0; i < screen_value.length; i++) {
+  for (let i = 0; i < memory.length; i++) {
     // we then have to check type of each value
-    const type = screen_value[i].type;
-    const value = screen_value[i].value;
+    const type = memory[i].type;
+    const value = memory[i].value;
 
     if (type == "number") {
       // if it's a number we push into the stach
@@ -203,10 +191,10 @@ function evaluatePostFix() {
 function evaluatePreFix() {
   const prefix_stack = [];
   //we nee to loop over the array of values from right to left
-  for (let i = screen_value.length - 1; i >= 0; i--) {
+  for (let i = memory.length - 1; i >= 0; i--) {
     // we then have to check type of each value
-    const type = screen_value[i].type;
-    const value = screen_value[i].value;
+    const type = memory[i].type;
+    const value = memory[i].value;
 
     if (type == "number") {
       // if it's a number we push into the stach
@@ -233,23 +221,42 @@ function evaluatePreFix() {
   return prefix_stack[0];
 }
 
-function closeButton() {
-  document.getElementById("tutorial").style.display = "none"
-}
-
-function openButton() {
-   document.getElementById("tutorial").style.display = "flex"
-}
-
-function toggleMode() {
-  
-  is_darkmode = !is_darkmode;
-  if(is_darkmode){
-    document.getElementById("calculator").classList.remove("light")
-  }else {
-    document.getElementById("calculator").classList.add("light")
-    
+function equalClick() {
+  // the operators have to be less than the numbers by 1
+  const numbers_count = memory.filter((elem) => elem.type == "number").length;
+  const operators_count = memory.length - numbers_count;
+  if (operators_count != numbers_count - 1) {
+    return;
   }
 
+  if (memory.length == 0) {
+    console.log("if the user has not entred anything yet");
+    return;
+  }
+  
+  if (temp_number != "") {
+    console.log("if the user has not pressed enter while inputing a number");
+    return;
+  }
 
+  if (is_postfix && memory[memory.length - 1].type == "number") {
+    console.log("it is a postfix and the last item is a number");
+    return;
+  }
+
+  if (!is_postfix && memory[memory.length - 1].type == "operator") {
+    console.log("it is a prefix and the last item is an operator");
+    return;
+  }
+
+  if (is_postfix) {
+    const result = evaluatePostFix();
+    displayResult(result);
+  } else {
+    const result = evaluatePreFix();
+    displayResult(result);
+  }
+
+  is_showing_results = true;
 }
+
